65a52735fe7b3c069fc3faa9f2e84b372326df52
xoxoinny0 <xoxoinny0@gmail.com>
2024-05-29 15:14:29 +0900

    #DEV#MOD - date-fns 추가

--- src/app/(inner)/(afterLogin)/_component/Datepicker.tsx
+++ src/app/(inner)/(afterLogin)/_component/Datepicker.tsx
@@ -1,48 +1,9 @@
 import { useState } from "react";
 import DatePicker, { registerLocale } from "react-datepicker";
-import dayjs from "dayjs";
 import "react-datepicker/dist/react-datepicker.css";
-import "dayjs/locale/ko";
-import localeData from "dayjs/plugin/localeData";
+import { ko } from "date-fns/locale/ko";
 
-dayjs.locale("ko");
-dayjs.extend(localeData);
-
-const getDayjsLocale = (): any => {
-  const localeData = dayjs().localeData();
-  return {
-    code: "ko",
-    formatDistance: () => "",
-    formatRelative: () => "",
-    localize: {
-      day: (n: number) => localeData.weekdaysShort()[n],
-      month: (n: number) => localeData.months()[n],
-      dayPeriod: () => "",
-      era: () => "",
-      ordinalNumber: () => 0,
-      quarter: () => 0,
-    },
-    formatLong: {
-      date: () => "yyyy-MM-dd",
-      time: () => "HH:mm",
-      dateTime: () => "yyyy-MM-dd HH:mm",
-    },
-    match: {
-      ordinalNumber: () => null,
-      era: () => null,
-      quarter: () => null,
-      month: () => null,
-      day: () => null,
-      dayPeriod: () => null,
-    },
-    options: {
-      weekStartsOn: 0,
-      firstWeekContainsDate: 1,
-    },
-  };
-};
-
-registerLocale("ko", getDayjsLocale());
+registerLocale("ko", ko);
 
 export default function Datepicker() {
   const [startDate, setStartDate] = useState<Date | null>(new Date());
