5e0a6c7868a304c5eecc17b4117ccaed40486469
xoxoinny0 <xoxoinny0@gmail.com>
2024-06-10 15:00:15 +0900

    #DEV#MOD - sidebar 링크 변경

--- src/app/(web)/(afterLogin)/_component/sidebar/SidebarMenuList.tsx
+++ src/app/(web)/(afterLogin)/_component/sidebar/SidebarMenuList.tsx
@@ -1,12 +1,17 @@
-import { HiMiniUserGroup } from "react-icons/hi2";
-import { MdUnsubscribe } from "react-icons/md";
 import SidebarMenuItem from "./SidebarMenuItem";
 
 export default function SidebarMenuList() {
   return (
     <ul>
-      <SidebarMenuItem svg={<HiMiniUserGroup />} text="회원정보" href="/user" />
-      <SidebarMenuItem svg={<MdUnsubscribe />} text="구독정보" href="/subscribe" />
+      <li>1xxxx 공통화면 </li>
+      <li>2xxxx 메인 거래소 화면 </li>
+      <SidebarMenuItem svg={"20100"} text="회사관리" href="/corp" />
+      <SidebarMenuItem svg={"20110"} text="사원관리" href="/empl" />
+      <SidebarMenuItem svg={"20200"} text="코인관리" href="/coin" />
+      <SidebarMenuItem svg={"20210"} text="코인거래내역" href="/coin/transaction" />
+      <li>3xxxx 중개사 화면 </li>
+      <SidebarMenuItem svg={"20110"} text="사원관리" href="/empl?group=brok" />
+      <li>4xxxx 하위 거래소 화면</li>
     </ul>
   );
 }
