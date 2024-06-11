d0675b995569fff40d4bc8c3a1cd5da9e9ca119a
yiminwook <grs0416@gmail.com>
2024-06-11 15:53:50 +0900

    #DEV#BUG -  AgGrid css

--- src/app/(web)/(afterLogin)/layout.tsx
+++ src/app/(web)/(afterLogin)/layout.tsx
@@ -1,16 +1,14 @@
+// css 순서변경 금지
+import "@/style/agGrid.css";
+import "@/style/datepicker.css";
+import * as style from "./layout.css";
+
 import { PropsWithChildren } from "react";
 import Header from "./_component/header/Header";
 import SidebarLayout from "./_component/sidebar/SidebarLayout";
-
-// css 순서변경 금지
-import "ag-grid-community/styles/ag-grid.css";
-import "ag-grid-community/styles/ag-theme-alpine.css";
-import "@/style/agCustomTheme.css";
-import "@/style/datepicker.css";
-import * as style from "./layout.css";
 import Sidebar from "./_component/sidebar/Sidebar";
 
-export default function Layout({ children }: PropsWithChildren) {
+export default async function Layout({ children }: PropsWithChildren) {
   return (
     <div className={style.col}>
       <Header />
