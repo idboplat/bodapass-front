310b964c75aab9c2d240bf612e6c2168d9767030
xoxoinny0 <xoxoinny0@gmail.com>
2024-06-14 15:03:40 +0900

    #DEV#MOD - 회사생성 모달 변경

--- src/app/(web)/(afterLogin)/corp/_component/CreateCorpModal.tsx
+++ src/app/(web)/(afterLogin)/corp/_component/CreateCorpModal.tsx
@@ -1,35 +1,37 @@
+import DefaultInput from "@/app/_component/input/DefaultInput";
 import Modal from "@/app/_component/modal/Modal";
-import { ModalProps } from "@/app/_lib/modalStore";
+import ModalCloseBtn from "@/app/_component/modal/ModalCloseBtn";
 import * as style from "@/app/_component/modal/modal.css";
 import { modalDefaultBtn } from "@/app/_component/modal/modalBtn.css";
-import ModalCloseBtn from "@/app/_component/modal/ModalCloseBtn";
+import TextSelect from "@/app/_component/select/TextSelect";
+import { ModalProps } from "@/app/_lib/modalStore";
+import { CORP_GRP, CORP_GRP_TP } from "@/type/common";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
-import LabelInput from "@/app/_component/input/LabelInput";
-import TextSelect from "@/app/_component/select/TextSelect";
-import { checkBoxDiv, inputBox } from "./createCorpModal.css";
-import CheckBox from "@/app/_component/input/CheckBox";
+import { inputBox, label } from "./createCorpModal.css";
 
 const ID = "createCorpModal";
 
 enum CreateCorpInput {
-  type = ID + "Type",
-  corpName = ID + "CorpName",
+  corpGrpTp = ID + "Type",
+  corpNm = ID + "CorpNm",
+  mastCorpCd = ID + "MastCorpCd",
   pw = ID + "Pw",
 }
 
+type corpGrpType = "G1" | "G2" | "G3" | "G4";
 interface CreateCorpModalProps {}
 
 export default function CreateCorpModal({ onClose, onSuccess }: ModalProps<CreateCorpModalProps>) {
-  const [type, setType] = useState<string | null>(null);
-  const [corpName, setCorpName] = useState("");
-  const [pw, setPw] = useState("");
-  const [pwCheck, setPwCheck] = useState(false);
+  const [corpGrpTp, setCorpGrpTp] = useState<corpGrpType | null>(null);
+  const [corpNm, setCorpNm] = useState("");
+  const [mastCorpCd, setMastCorpCd] = useState("");
+  const [pwCheck] = useState(false);
 
   const mutation = useMutation({
     mutationKey: ["서비스 아이디"],
     mutationFn: async () => {
-      console.table({ type, corpName: corpName.trim(), pw });
+      console.table({ corpGrpTp, corpNm: corpNm.trim(), mastCorpCd });
     },
   });
 
@@ -44,21 +46,24 @@ export default function CreateCorpModal({ onClose, onSuccess }: ModalProps<Creat
     }
   };
 
+  const getCorpGrpTpItems = (corpGrpTp: CORP_GRP_TP) => {
+    const idx = CORP_GRP.findIndex((v) => v === corpGrpTp);
+    return CORP_GRP.slice(idx + 1);
+  };
+
   const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
     switch (id) {
-      case CreateCorpInput.corpName:
-        setCorpName(() => value);
+      case CreateCorpInput.corpNm:
+        setCorpNm(() => value);
         break;
-      case CreateCorpInput.pw:
-        setPw(() => value);
+      case CreateCorpInput.mastCorpCd:
+        setMastCorpCd(() => value);
         break;
     }
   };
 
-  const onChangeSelect = (value: string) => setType(() => value);
-
-  const togglePwCheck = () => setPwCheck((pre) => !pre);
+  const onChangeSelect = (value: string) => setCorpGrpTp(() => value as corpGrpType);
 
   return (
     <Modal id={ID} onClose={onClose}>
@@ -66,40 +71,51 @@ export default function CreateCorpModal({ onClose, onSuccess }: ModalProps<Creat
         <ModalCloseBtn onClose={onClose} />
         <div>
           <div className={style.modalHeader}>
-            <h3 className={style.modalTitle}>회사등록</h3>
+            <h3 className={style.modalTitle}>회사 등록</h3>
           </div>
           <div className={inputBox}>
             <TextSelect
-              value={type}
+              value={corpGrpTp}
               onChange={onChangeSelect}
-              items={["중개", "거래소"]}
-              placeholder="회사유형 선택"
+              items={getCorpGrpTpItems("G1")}
+              placeholder="회사그룹 구분"
               style={{
-                height: 35,
+                height: 35.6,
               }}
             />
           </div>
           <div className={inputBox}>
-            <LabelInput
-              label="회사명"
-              value={corpName}
-              id={CreateCorpInput.corpName}
+            <label className={label} htmlFor={CreateCorpInput.corpNm}>
+              회사명
+            </label>
+            <DefaultInput
+              id={CreateCorpInput.corpNm}
+              value={corpNm}
               onChange={onChangeInput}
-              onReset={() => setCorpName("")}
+              onReset={() => setCorpNm("")}
+            />
+          </div>
+
+          <div className={inputBox}>
+            <label className={label} htmlFor={CreateCorpInput.mastCorpCd}>
+              주 회사 코드
+            </label>
+            <DefaultInput
+              value={mastCorpCd}
+              id={CreateCorpInput.mastCorpCd}
+              onChange={onChangeInput}
+              onReset={() => setMastCorpCd("")}
             />
           </div>
           <div className={inputBox}>
-            <LabelInput
-              label="Main 관리자 비밀번호"
-              value={pw}
+            <label className={label} htmlFor={CreateCorpInput.pw}>
+              Main 관리자 비밀번호
+            </label>
+            <DefaultInput
               id={CreateCorpInput.pw}
               onChange={onChangeInput}
               type={pwCheck ? "text" : "password"}
-              onReset={() => setPw("")}
             />
-            <div className={checkBoxDiv}>
-              <CheckBox value={pwCheck} onClick={togglePwCheck} label={"비밀번호 확인"} />
-            </div>
           </div>
         </div>
         <div className={style.modalBtnBox}>
