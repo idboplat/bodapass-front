151a8909eb0bc5ed9e55b74963683fdef32d1acb
xoxoinny0 <xoxoinny0@gmail.com>
2024-06-24 18:39:24 +0900

    #DEV#MOD - 로그인 시 sessionId, sessionKey값 수정

--- src/model/nextAuth/providers.ts
+++ src/model/nextAuth/providers.ts
@@ -6,22 +6,21 @@ export const credentialAuthorize: CredentialAuthorize = async (_credentials, req
   try {
     const credentials = _credentials as unknown as Credentials;
     if (!credentials) return null;
-    const B0001ARes = await callTms<TBW_000001_P01>({
+    const TBW_000001_P01Res = await callTms<TBW_000001_P01>({
       svcId: "TBW_000001_P01",
       data: [credentials.email, credentials.password, credentials.corpCode],
       session: null,
     });
-    const B0001AData = B0001ARes.svcRspnData;
-    if (B0001AData === null) {
+    const TBW_000001_P01Data = TBW_000001_P01Res.svcRspnData;
+    if (TBW_000001_P01Data === null) {
       throw new Error("관리자에게 문의하세요");
     }
-    console.log("B0001AData", B0001AData[0]);
 
-    const { F01, F02, F05 } = B0001AData[0];
+    const { F03, F04, F05 } = TBW_000001_P01Data[0];
     return {
       id: credentials.email,
-      sessionId: F01,
-      sessionKey: F02,
+      sessionId: F03,
+      sessionKey: F04,
       errorMsg: undefined,
       corpCd: F05,
     };
