83baaea6da7301e29611aadddc843faaa42f1eb9
xoxoinny0 <xoxoinny0@gmail.com>
2024-06-24 16:30:29 +0900

    #DEV#MOD - 사이드바 css 변경

--- src/app/(web)/(afterLogin)/_component/sidebar/sidebar.css.ts
+++ src/app/(web)/(afterLogin)/_component/sidebar/sidebar.css.ts
@@ -49,8 +49,8 @@ export const menuItem = style({
       backgroundColor: "#ebebe7",
     },
     "&.active ": {
-      fontWeight: 900,
-      // backgroundColor: "#ebebe7",
+      fontWeight: 700,
+      backgroundColor: "#ebebe7",
     },
   },
 });
