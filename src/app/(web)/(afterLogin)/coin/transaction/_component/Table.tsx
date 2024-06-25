f0706cc24b42e4713c8c539c48f056c1aaf75e13
xoxoinny0 <xoxoinny0@gmail.com>
2024-06-25 14:17:00 +0900

    #DEV#MOD - transaction 페이지 서비스 연결

--- src/app/(web)/(afterLogin)/coin/transaction/_component/Table.tsx
+++ src/app/(web)/(afterLogin)/coin/transaction/_component/Table.tsx
@@ -9,7 +9,7 @@ import { ICellRendererParams } from "ag-grid-community";
 import ReqStatus from "./ReqStatus";
 import { useQuery } from "@tanstack/react-query";
 import callTms from "@/model/callTms";
-import { TBW_000300_Q01 } from "@/type/api";
+import { TBW_001000_Q01 } from "@/type/api";
 import { Session } from "next-auth";
 
 export default function Table({ session }: { session: Session }) {
@@ -24,35 +24,39 @@ export default function Table({ session }: { session: Session }) {
   });
 
   const { data } = useQuery({
-    queryKey: ["TBW_000300_Q01"],
+    queryKey: ["TBW_001000_Q01"],
     queryFn: async () => {
-      const TBW_000300_Q01Res = await callTms<TBW_000300_Q01>({
+      const TBW_001000_Q01Res = await callTms<TBW_001000_Q01>({
         session,
-        svcId: "TBW_000300_Q01",
-        data: [""], // 입출고 구분
+        svcId: "TBW_001000_Q01",
+        data: [session.user.corpCd, "", "", "", "", ""],
         pgSize: 20,
       });
-      const TBW_000300_Q01Data = TBW_000300_Q01Res.svcRspnData || [];
-      return TBW_000300_Q01Data;
+      const TBW_001000_Q01Data = TBW_001000_Q01Res.svcRspnData || [];
+      return TBW_001000_Q01Data;
     },
     select: (data) => {
       const result = data.map((item) => ({
         회사코드: item.F01,
         "입출고 일자": item.F02,
         "입출고 일련번호": item.F03,
-        종목코드: item.F04,
-        "입출고 구분": item.F05,
-        "입출고 수량": item.F06,
-        "생성 작업 ID": item.F07,
-        "생성 작업 일시": item.F08,
-        "변경 작업 ID": item.F09,
-        "변경 작업 일시": item.F10,
+        "계좌 번호": item.F04,
+        "종목 코드": item.F05,
+        "입출고 구분": item.F06,
+        "입출고 적요 구분": item.F07,
+        "입출고 수량": item.F08,
+        "잔고 수량": item.F09,
+        "신청 상태 구분": item.F10,
+        "생성 작업 ID": item.F11,
+        "생성 작업 일시": item.F12,
+        "변경 작업 ID": item.F13,
+        "변경 작업 일시": item.F14,
       }));
       return result;
     },
   });
 
-  console.log("TBW_000300_Q01Data", data);
+  console.log("TBW_001000_Q01Data", data);
 
   const dummy = [
     {
