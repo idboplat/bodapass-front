50fee8e484424180e51cab4feea83ec249dd5d8c
xoxoinny0 <xoxoinny0@gmail.com>
2024-06-25 11:43:40 +0900

    #DEV#MOD - calltms svc type 정리

--- src/type/api/index.ts
+++ src/type/api/index.ts
@@ -93,3 +93,61 @@ export type TBW_000200_P04 = RspnData<{
 export type TBW_000300_P01 = RspnData<{
   F01: Filler;
 }>;
+
+/** 계좌 잔고(코인선물) 입출고(G1) 조회  */
+export type TBW_000300_Q01 = RspnData<{
+  F01: string;
+  F02: string;
+  F03: string;
+  F04: string;
+  F05: string;
+  F06: string;
+  F07: string;
+  F08: string;
+  F09: string;
+  F10: string;
+}>;
+
+/** 그룹 코드(회사)(공통) 조회  */
+export type TBW_000000_R01 = RspnData<{
+  F01: string;
+  F02: string;
+  F03: string;
+  F04: string;
+  F05: string;
+  F06: string;
+  F07: string;
+  F08: string;
+}>;
+
+/** 그룹 코드(사원)(공통) 조회  */
+export type TBW_000001_Q01 = RspnData<{
+  F01: string;
+  F02: string;
+  F03: string;
+  F04: string;
+  F05: string;
+  F06: string;
+  F07: string;
+  F08: string;
+  F09: string;
+  F10: string;
+}>;
+
+/** 계좌 입출고(코인선물) 내역 조회  */
+export type TBW_001000_Q01 = RspnData<{
+  F01: string;
+  F02: string;
+  F03: string;
+  F04: string;
+  F05: string;
+  F06: string;
+  F07: string;
+  F08: string;
+  F09: string;
+  F10: string;
+  F11: string;
+  F12: string;
+  F13: string;
+  F14: string;
+}>;
