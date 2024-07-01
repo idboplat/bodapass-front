772dc4e55338108ad0e8a007b1adede12255c628
yiminwook <grs0416@gmail.com>
2024-07-01 10:19:48 +0900

    #DEV#NEW - 200502 200503 메타데이터 추가

--- src/app/(web)/(afterLogin)/_component/balance/transaction/Home.tsx
+++ src/app/(web)/(afterLogin)/_component/balance/transaction/Home.tsx
@@ -3,6 +3,8 @@ import Nav from "./_component/Nav";
 import Table from "./_component/Table";
 import { GRID_100502_COLS } from "./_const/100502";
 import { GRID_100503_COLS } from "./_const/100503";
+import { GRID_200502_COLS } from "./_const/200502";
+import { GRID_200503_COLS } from "./_const/200503";
 import * as css from "./home.css";
 
 interface TransactionHomeProps {}
@@ -13,6 +15,8 @@ export default async function TransactionHome({ session, page }: HomeProps<Trans
   const map: Record<string, Meta> = {
     "100502": { cols: GRID_100502_COLS, svcId: "TBW_001000_Q01" },
     "100503": { cols: GRID_100503_COLS, svcId: "TBW_001000_R01" },
+    "200502": { cols: GRID_200502_COLS, svcId: "TBW_001000_Q01" },
+    "200503": { cols: GRID_200503_COLS, svcId: "TBW_001000_R01" },
   };
 
   const meta = map[page.number];
