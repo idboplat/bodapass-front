ef7a8f431fe1e750a48041e8d117929158111e2f
yiminwook <grs0416@gmail.com>
2024-12-27 13:53:59 +0900

    #DEV#NEW - theme colors

--- src/types/global/variable.d.ts
+++ src/types/global/variable.d.ts
@@ -1,6 +1,7 @@
-declare module '@variable' {
+declare module "@variable" {
   export const bezierCurve: string;
   export const boxShadow: string;
+  export const backdropColor: string;
 
   export const breakpointSm: string;
   export const breakpointXs: string;
@@ -8,9 +9,37 @@ declare module '@variable' {
   export const breakpointLg: string;
   export const breakpointXl: string;
 
+  export const mobileBoundary: string;
+
   export const zIndexApp: string;
   export const zIndexModal: string;
   export const zIndexPopover: string;
   export const zIndexOveray: string;
   export const zIndexMax: string;
+
+  export const fontPretendard: string;
+
+  export const colorsWhite0: string;
+  export const colorsWhite1: string;
+  export const colorsWhite2: string;
+  export const colorsWhite3: string;
+  export const colorsWhite4: string;
+  export const colorsBlack0: string;
+  export const colorsBlack1: string;
+  export const colorsBlack2: string;
+  export const colorsGray0: string;
+  export const colorsGray1: string;
+  export const colorsGray2: string;
+  export const colorsGray3: string;
+  export const colorsGray4: string;
+  export const colorsGray5: string;
+  export const colorsGray6: string;
+  export const colorsRed0: string;
+  export const colorsRed1: string;
+  export const colorsBlue0: string;
+  export const colorsBlue1: string;
+  export const colorsBlue2: string;
+  export const colorsGreen0: string;
+  export const colorsGreen1: string;
+  export const colorsPurple0: string;
 }
