24147ba97100c02da1613ec753911c3f3c9b3020
yiminwook <grs0416@gmail.com>
2024-12-30 09:42:08 +0900

    #DEV#BUG - typo

--- src/types/global/mantine.d.ts
+++ src/types/global/mantine.d.ts
@@ -2,6 +2,7 @@ import { DefaultMantineColor, MantineColorsTuple } from "@mantine/core";
 
 // color를 추가하고 싶다면 아래와 같이 추가하면 된다.
 type ExtendedCustomColors = "demo" | DefaultMantineColor;
+type ExtendedButtonVariant = "demoVarient" | ButtonVariant;
 
 declare module "@mantine/core" {
   export interface MantineThemeColorsOverride {
