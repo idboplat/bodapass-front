6249217acf2ff1ba8134d19002caca21014d3d2f
xoxoinny0 <xoxoinny0@gmail.com>
2025-01-02 10:58:15 +0900

    #DEV#MOD - DatePicker props 수정

--- src/components/common/datepicker/CustomDatePicker.tsx
+++ src/components/common/datepicker/CustomDatePicker.tsx
@@ -1,26 +1,39 @@
-import { DatePickerInput } from "@mantine/dates";
+import { CSSProperties, StylesApiProps } from "@mantine/core";
+import { DatePickerInput, DatePickerInputFactory } from "@mantine/dates";
 import { Dispatch, SetStateAction } from "react";
 
 interface CustomDatePickerProps {
-  id: string;
-  height: number;
+  id?: string;
+  classNames?: StylesApiProps<DatePickerInputFactory>["classNames"];
+  inputStyle?: CSSProperties;
+  popOverStyle?: CSSProperties;
+  height?: number;
   value: [Date | null, Date | null];
   setValue: Dispatch<SetStateAction<[Date | null, Date | null]>>;
 }
 
-export default function CustomDatePicker({ id, height, value, setValue }: CustomDatePickerProps) {
+export default function CustomDatePicker({
+  id,
+  classNames,
+  inputStyle,
+  popOverStyle,
+  height = 47,
+  value,
+  setValue,
+}: CustomDatePickerProps) {
   return (
     <DatePickerInput
       id={id}
+      classNames={classNames}
       type="range"
       valueFormat="YYYY-MM-DD"
       value={value}
       onChange={setValue}
       numberOfColumns={2}
       radius={15}
-      styles={{ input: { textAlign: "center", height } }}
+      styles={{ input: { textAlign: "center", height, ...inputStyle } }}
       popoverProps={{
-        styles: { dropdown: { borderRadius: 15, borderColor: "#588CBF" } },
+        styles: { dropdown: { borderRadius: 15, borderColor: "#588CBF", ...popOverStyle } },
       }}
       labelSeparator="~"
     />
