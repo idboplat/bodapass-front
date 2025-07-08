6d52b9d28d0490df3c3c128ebf92809540e30c16
Minwook Yi <grs0416@gmail.com>
2025-07-08 22:31:22 +0900

    #DEV#MOD - 404페이지 뒤로가기 버튼

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,20 @@
-import { Metadata } from "next";
+"use client";
+import { useRouter } from 'next/navigation';
+import { Button } from "@mantine/core";
 
 export default function Notfound() {
+  const router = useRouter();
+
+  const onClickBack = () => {
+    router.back();
+  };
+
   return (
-    <div>
+    <section className="main">
       <h2>요청하신 페이지를 찾을 수 없습니다.</h2>
-    </div>
+      <div>
+        <Button onClick={onClickBack} mx="auto" display="block">뒤로 돌아가기</Button>
+      </div>
+    </section>
   );
 }
-
-export const metadata: Metadata = {
-  title: "404 | Not Found",
-};
