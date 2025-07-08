91e19373d113c60fc8f29192ba24962ca1127fc8
Minwook Yi <grs0416@gmail.com>
2025-07-08 22:19:53 +0900

    #DEV#MOD - 데모 페이지 헤더 제거

--- src/app/(web)/demo/page.tsx
+++ src/app/(web)/demo/page.tsx
@@ -1,20 +1,12 @@
 "use client";
 import Link from "next/link";
 import css from "./page.module.scss";
-import BackHeader from "@/components/common/header/BackHeader";
-import { useRouter } from "next/navigation";
 import { clsx } from "clsx";
 
 export default function Page() {
-  const router = useRouter();
-
-  const onClickBack = () => {
-    router.back();
-  };
 
   return (
     <>
-      <BackHeader title="Demo" onClickBack={onClickBack} />
       <div className={clsx(css.wrap)}>
         <div className={css.box}>
           <Link href="/demo/ocr" className={css.link}>
