a381b51e2d52cd9860fd2f840d5e8c8f8201cc4b
yiminwook <grs0416@gmail.com>
2025-08-29 17:20:45 +0900

    #TST#NEW - 출퇴근 처리

--- src/pages/[locale]/attendance.tsx
+++ src/pages/[locale]/attendance.tsx
@@ -3,14 +3,18 @@ import { Authorized } from "@/libraries/auth/authorized";
 import { useSession } from "@/libraries/auth/use-session";
 import { callTms, StringRspnData, tmsApi } from "@/libraries/call-tms";
 import { makeStaticProps, getStaticPaths } from "@/libraries/i18n/get-static";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import css from "./attendance.module.scss";
+import { Button } from "@mantine/core";
+import { toast } from "sonner";
 
 const getEmployeesByCorpCd = async ({ session }: { session: Session }) => {
+  // teamleader1@gmail.com 고정 테스트
   const result = await callTms<StringRspnData<10>>({
     svcId: "TCM200101SMQ01",
     session,
     locale: "ko",
-    data: [session.corpCd, "25800005", session.userId],
+    data: ["25800002", "25800005", ""],
   });
 
   const data = result.svcRspnData || [];
@@ -23,6 +27,9 @@ const getEmployeesByCorpCd = async ({ session }: { session: Session }) => {
     ordrPrc: d.F05,
     wrkStrDd: d.F06,
     wrkEndDd: d.F07,
+    cntr: d.F08 === "Y",
+    ins: d.F09 === "Y",
+    userNm: d.F10,
   }));
 };
 
@@ -38,44 +45,92 @@ export default function Attendance() {
 
 const Content = () => {
   const { data: session } = useSession();
+  const queryClient = useQueryClient();
 
   if (!session) throw new Error("Session is not found");
 
   const { data: query } = useQuery({
-    queryKey: ["TCM200101SMQ01"],
+    queryKey: ["TCM200101SMQ01", session],
     queryFn: () => getEmployeesByCorpCd({ session }),
   });
 
-  const { data: query2 } = useQuery({
-    queryKey: ["image"],
-    queryFn: () => {
-      const json = tmsApi
-        .post("api/call_tms_svc", {
-          json: {
-            apiTranKey: 1,
-            apiLangCd: "KO",
-            apiCorpCd: session.corpCd,
-            apiUserId: session.userId,
-            svcRqstList: [
-              {
-                svcTranKey: 1,
-                svcId: "TCM900001SSQ99",
-                svcIntfcVer: "1.0.0",
-                svcMdtyYn: true,
-                svcRqstPageSize: 1,
-                svcRqstPageSn: 1,
-                svcRqstData: [{ F01: "25800005", F02: "25800005" }],
-              },
-            ],
-          },
-        })
-        .json();
-      nativeLogger(JSON.stringify(json, null, 2));
-      return json;
+  const { mutate: updateAttendance, isPending } = useMutation({
+    mutationFn: (args: {
+      masterCorpCd: string;
+      corpCd: string;
+      userId: string;
+      attCd: "I" | "O" | "A";
+    }) =>
+      callTms<StringRspnData<1>>({
+        svcId: "TCM200101SSP01",
+        session,
+        locale: "ko",
+        data: [args.masterCorpCd, args.corpCd, args.userId, args.attCd],
+      }),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["TCM200101SMQ01"] });
+      toast.success("출퇴근 정보가 업데이트되었습니다.");
     },
   });
 
-  return <></>;
+  return (
+    <div className={css.wrap}>
+      <div className={css.list}>
+        {query?.map((d) => (
+          <div key={d.userId} className={css.item}>
+            <div className={css.info}>
+              <div>주_회사코드: {d.mastCorpCd}</div>
+              <div>회사코드: {d.corpCd}</div>
+              <div>사용자ID: {d.userId}</div>
+              <div>종목_코드: {d.instCd}</div>
+              <div>주문_가격: {d.ordrPrc}</div>
+              <div>작업_시작_일자: {d.wrkStrDd}</div>
+              <div>작업_종료_일자: {d.wrkEndDd}</div>
+              <div>계약여부: {d.cntr ? "Y" : "N"}</div>
+              <div>보험여부: {d.ins ? "Y" : "N"}</div>
+              <div>사용자명: {d.userNm}</div>
+            </div>
+
+            <div className={css.buttonBox}>
+              <Button
+                variant="filled"
+                color="blue"
+                data-type="in"
+                loading={isPending}
+                onClick={() =>
+                  updateAttendance({
+                    masterCorpCd: d.mastCorpCd,
+                    corpCd: d.corpCd,
+                    userId: d.userId,
+                    attCd: "I",
+                  })
+                }
+              >
+                출근
+              </Button>
+
+              <Button
+                variant="filled"
+                color="red"
+                data-type="out"
+                loading={isPending}
+                onClick={() =>
+                  updateAttendance({
+                    masterCorpCd: d.mastCorpCd,
+                    corpCd: d.corpCd,
+                    userId: d.userId,
+                    attCd: "O",
+                  })
+                }
+              >
+                퇴근
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 const getStaticProps = makeStaticProps(["common"]);
