b1ec511560ec707bd1d9f16bdabc0cb2232b9116
xoxoinny0 <xoxoinny0@gmail.com>
2025-10-27 15:37:40 +0900

    #DEV#NEW - 구글스토어 다운로드 페이지 생성

---
+++ src/pages/[locale]/download.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import css from "./download.module.scss";
+
+export default function Download() {
+  return (
+    <div className={css.container}>
+      <div className={css.header}>
+        <Image src="/assets/images/logo.png" alt="Logo" width={200} height={200} />
+        <p className={css.firstText}>쉽고 간편한 건설노동자 관리! </p>
+        <p>
+          <strong>일당백</strong> 어플로 시작하세요.
+        </p>
+      </div>
+      <div className={css.content}>
+        <a
+          href="https://play.google.com/apps/internaltest/4701036027179610563"
+          className={css.DownloadAppBtn}
+          target="_blank"
+        >
+          <Image
+            src="/assets/svg/google-play-store-button.svg"
+            alt="Google Play"
+            width={250}
+            height={100}
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
