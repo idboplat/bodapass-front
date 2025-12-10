190f054c00441014d1be7115c08fd3e62cbf2d3f
Minwook Yi <grs0416@gmail.com>
2025-12-10 23:13:17 +0900

    TST#NEW - nexti18n

--- next-i18next.config.ts
+++ next-i18next.config.ts
@@ -1,21 +1,4 @@
 import { UserConfig } from "next-i18next";
-import path from "path";
-
-// Vercel 환경에서 올바른 경로 찾기
-const getServerLocalePath = () => {
-  if (typeof window !== "undefined") {
-    return "/locales"; // 클라이언트
-  }
-
-  const isVercel = process.env.VERCEL === "1";
-
-  if (isVercel) {
-    return path.resolve("./locales");
-  }
-
-  // 로컬 개발 환경
-  return path.resolve("./public/locales");
-};
 
 // https://github.com/vercel/next.js/discussions/47187
 export const i18nConfig: UserConfig = {
@@ -27,7 +10,6 @@ export const i18nConfig: UserConfig = {
   defaultNS: "common",
   fallbackLng: "ko",
   reloadOnPrerender: process.env.NODE_ENV === "development",
-  localePath: getServerLocalePath(),
 };
 
 export default i18nConfig;
