6a4b35d20fb3a695d76e894aca18e2b883e39a22
xoxoinny0 <xoxoinny0@gmail.com>
2025-12-29 16:56:34 +0900

    #DEV#MOD - 출근완료 페이지 퍼블리싱

--- src/components/attendance/complete-home.tsx
+++ src/components/attendance/complete-home.tsx
@@ -1,6 +1,7 @@
 import { useTCM200101SSQ01 } from "@/hooks/tms/use-attendance";
 import { useSession } from "@/libraries/auth/use-session";
-
+import ClapIcon from "/public/assets/svg/clap.svg";
+import css from "./complete-home.module.scss";
 interface Props {
   attendanceCount: number;
   mastCorpCd: string;
@@ -14,13 +15,16 @@ export default function CompleteHome({ attendanceCount, mastCorpCd, corpCd }: Pr
   const TCM200101SSQ01 = useTCM200101SSQ01({ session, mastCorpCd, corpCd, userId: session.userId });
 
   return (
-    <div className={"mobileLayout"}>
-      <div>출석 완료</div>
-
-      <div>{session.userNm} 반장님 고생하셨어요 :)</div>
-      <div>{TCM200101SSQ01.data?.corpNm} 현장</div>
-      <div>{attendanceCount}명의 출근이 완료되었습니다.</div>
-      <div>안전하게 근무 하세요.</div>
+    <div className={`${css.container} mobileLayout`}>
+      <div className={css.header}>
+        <div className={css.title}>{session.userNm} 반장님 고생하셨어요. :&#41;</div>
+        <div className={css.subtitle}>{TCM200101SSQ01.data?.corpNm} 현장</div>
+      </div>
+      <ClapIcon className={css.clapIcon} />
+      <div className={css.content}>
+        <div className={css.description}>{attendanceCount}명의 출근이 완료되었습니다.</div>
+        <div className={css.description}>안전하게 근무 하세요.</div>
+      </div>
     </div>
   );
 }
